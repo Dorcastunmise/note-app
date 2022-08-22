70e8269b761e3c0dd2c8ec694c37f367ee9701b1
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-22 10:51:36 +0100

    still working on key and id

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,7 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note) => (
         <div key={note.id}>
             <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
