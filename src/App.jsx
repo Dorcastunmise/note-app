76f477e3bc0cf5248da1f6c2b4e61c40b5bce2fa
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-27 16:21:39 +0100

    npm install react-mde --legacy-peer-deps

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,7 @@ export default function App() {
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
                     deleteNote={deleteNote}
+                    key={notes.id}
                 />
                 {
                     currentNoteId && 
